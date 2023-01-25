bbb480943c5990dcf7b5e17279ba83f245e6be2d
meke <3197801970@qq.com>
2023-01-25 14:17:48 +0800

    一些描述信息

---
+++ README.md
@@ -0,0 +1,15 @@
+# 哔哩的静态首页仿写
+
+## 概要
+
+这个项目是使用了 react 对哔哩的首也进行仿写（哔哩哔哩采用的是 vue 写的首页，我用 react 是想要学习使用 react）
+
+这个项目是第一次使用 react 所以写的时候从幼稚到逐渐会用的过程，能明显看出代码的风格改变
+
+## 预览
+
+![](https://raw.githubusercontent.com/mekefly/img/main/20221023184619.png)
+![](https://photo-frame.oss-cn-beijing.aliyuncs.com/202210231903999.png)
+![](https://photo-frame.oss-cn-beijing.aliyuncs.com/202210231856412.png)
+
+![](https://photo-frame.oss-cn-beijing.aliyuncs.com/202210231859333.png)
--- src/utils/flixible.ts
+++ src/utils/flixible.ts
@@ -1,5 +1,14 @@
 const minWindowWidth = 1060;
 const maxWindowWidth = 2650;
+/**
+ * 当屏幕宽度在 `minWindowWidth` 到 `maxWindowWidth` 之间时，将根据比例输出 `minNumber` 到 `maxNumber` 之间的数
+ *
+ * @export
+ * @param {number} minNumber 当屏幕宽度小于 minWindowWidth 时将输出 minNumber 这个数
+ * @param {number} maxNumber 当屏幕宽度小于 maxWindowWidth 时将输出 maxNumber 这个数
+ * @param {{ floor?: boolean }} [options={}]
+ * @return {*}
+ */
 export function flexibleRange(
   minNumber: number,
   maxNumber: number,
